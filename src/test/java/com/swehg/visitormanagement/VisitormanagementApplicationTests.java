5224c5a684b52d5f605465f4cc42c9ed93c32848
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-07 18:26:14 +0530

    initial commit

---
+++ src/test/java/com/swehg/visitormanagement/VisitormanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swehg.visitormanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitormanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
