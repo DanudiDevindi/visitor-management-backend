57a81ab7bdf175bde8c118796d9e57a433aa06ed
KavinduAkash <bkkasamarasingha1220@gmail.com>
2021-07-17 00:23:23 +0530

    extend by SpringBootServletInitializer

--- src/main/java/com/swehg/visitormanagement/VisitormanagementApplication.java
+++ src/main/java/com/swehg/visitormanagement/VisitormanagementApplication.java
@@ -2,9 +2,10 @@ package com.swehg.visitormanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VisitormanagementApplication {
+public class VisitormanagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VisitormanagementApplication.class, args);
